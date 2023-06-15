1ba973a4e16544c1854306b07f42b0280a5ea5ea
jaxson-lybbert <jax.a.lybb@gmail.com>
2023-06-15 19:37:56 -0400

    Adjusted SVG point positioning for text and shape rendering

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -7,7 +7,7 @@ describe("Shapes", () => {
       const shape = new Triangle();
       shape.setColor("blue");
       expect(shape.render()).toEqual(
-        '<polygon points="150, 18 244, 182 56, 182" fill="blue"/>'
+        '<polygon points="0, 300 100, 0 200, 300" fill="blue"/>'
       );
     });
   });
@@ -17,7 +17,7 @@ describe("Shapes", () => {
       const shape = new Square();
       shape.setColor("white");
       expect(shape.render()).toEqual(
-        '<rect x="20" y="10" width="200" height="200" fill="white"/>'
+        '<rect x="20" y="20" width="200" height="200" fill="white"/>'
       );
     });
   });
@@ -27,7 +27,7 @@ describe("Shapes", () => {
       const shape = new Circle();
       shape.setColor("red");
       expect(shape.render()).toEqual(
-        '<circle cx="25" cy="75" r="100" fill="red"/>'
+        '<circle cx="50%" cy="50%" r="100" fill="red"/>'
       );
     });
   });
@@ -35,10 +35,10 @@ describe("Shapes", () => {
 
 describe("SVG", () => {
   describe("Render logo text", () => {
-    it("Should render text with proper color", () => {
+    it("Should render text with proper color for a square logo", () => {
       const logoText = new SVG("JAL", "blue");
       expect(logoText.renderText()).toEqual(
-        '<text x="100" y="100" font-family="Verdana" font-size="35" fill="blue">JAL</text>'
+        '<text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" font-family="Verdana" font-size="35" fill="blue">JAL</text>'
       );
     });
   });
