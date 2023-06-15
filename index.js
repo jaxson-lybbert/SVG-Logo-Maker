fba063cd51cc5c0ed812fbdde3acd1af9ba32422
jaxson-lybbert <jax.a.lybb@gmail.com>
2023-06-15 18:59:02 -0400

    All shapes render test pass

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const path = require("path");
-const { Triangle, Square, Circle } = require("../lib/shapes");
+const { Triangle, Square, Circle } = require("./lib/shapes");
 
 // Gets user input for logo design
 inquirer
--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -12,23 +12,23 @@ describe("Shapes", () => {
     });
   });
   // Test to render new Square
-  //   describe("Render new Square", () => {
-  //     it("Should render a new .svg square when the user selects square", () => {
-  //       const shape = new Square();
-  //       shape.setColor("white");
-  //       expect(shape.render()).toEqual(
-  //         '<rect x="20" y="10" width="200" height="200" fill="white"/>'
-  //       );
-  //     });
-  //   });
-  //   // Test to render new Circle
-  //   describe("Render new Circle", () => {
-  //     it("Should render a new .svg circle when the user selects circle", () => {
-  //       const shape = new Circle();
-  //       shape.setColor("red");
-  //       expect(shape.render()).toEqual(
-  //         '<circle cx="25" cy="75" r="100" fill="red"/>'
-  //       );
-  //     });
-  //   });
+  describe("Render new Square", () => {
+    it("Should render a new .svg square when the user selects square", () => {
+      const shape = new Square();
+      shape.setColor("white");
+      expect(shape.render()).toEqual(
+        '<rect x="20" y="10" width="200" height="200" fill="white"/>'
+      );
+    });
+  });
+  // Test to render new Circle
+  describe("Render new Circle", () => {
+    it("Should render a new .svg circle when the user selects circle", () => {
+      const shape = new Circle();
+      shape.setColor("red");
+      expect(shape.render()).toEqual(
+        '<circle cx="25" cy="75" r="100" fill="red"/>'
+      );
+    });
+  });
 });
